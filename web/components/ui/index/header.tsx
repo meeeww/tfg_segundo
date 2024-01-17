6032677b3f3627dde0c38f2c2c4c042e81e13a8b
Omar <118843137+Rakions@users.noreply.github.com>
2024-01-17 18:13:18 +0100

    Terminar header

--- web/components/ui/index/header.tsx
+++ web/components/ui/index/header.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
 import HeaderLinks from "./headerLinks";
 
 const Header = () => {
@@ -25,14 +17,16 @@ const Header = () => {
   ];
 
   return (
-    <header className="w-full h-[75px] flex items-center justify-between md:justify-center gap-10 px-4 relative">
-      <h2 className="text-2xl font-bold">ChitChat</h2>
+    <header className="w-full h-[75px] flex items-center justify-between md:justify-center gap-10 px-4 relative bg-sky-50">
+      <h2 className="text-2xl font-bold">
+        Chit<span className="text-sky-500">Chat</span>
+      </h2>
       <HeaderLinks links={enlaces} />
       <Button className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-4 py-2 rounded-md hidden md:block">
         Get Started
       </Button>
-      <Button className="block md:hidden" onClick={mobileMenu}>
-        menu
+      <Button className="block md:hidden bg-[none] hover:bg-[none]" onClick={mobileMenu}>
+        <Menu className="text-black" />
       </Button>
     </header>
   );
--- web/components/ui/index/headerLinks.tsx
+++ web/components/ui/index/headerLinks.tsx
@@ -13,7 +13,7 @@ interface props {
 
 const HeaderLinks: React.FC<props> = ({ links }) => {
   return (
-    <ul className="gap-4 flex md:flex-row flex-col md:static absolute top-0 left-0 w-full md:w-auto md:bg-white bg-slate-100 mt-[70px] md:mt-0 items-center justify-center translate-x-[-100%] md:translate-x-0 menu transition-all p-4">
+    <ul className="gap-4 flex md:flex-row flex-col md:static absolute top-0 left-0 w-full md:w-auto bg-sky-50 mt-[70px] md:mt-0 items-center justify-center translate-x-[-100%] md:translate-x-0 menu transition-all p-4">
       {links.map((link, index) => (
         <Link
           key={index}
