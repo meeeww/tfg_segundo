48ee056118cb38c228fab7301a268753c2fc1996
Omar <118843137+Rakions@users.noreply.github.com>
2024-01-17 01:09:27 +0100

    Agregar componente <Button/> a header.tsx
    Importar componente dropdown-menu.tsx

--- web/components/ui/index/header.tsx
+++ web/components/ui/index/header.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
-  const toggleMenu = () => {
-    console.log("menu");
-  };
-
   return (
     <header className="w-full h-[75px] flex items-center justify-between md:justify-center gap-10 px-4 relative">
       <h2 className="text-2xl font-bold">ChitChat</h2>
@@ -24,12 +29,9 @@ export default function Header() {
           Contact
         </Link>
       </ul>
-      <button className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-4 py-2 rounded-md hidden md:block">
+      <Button className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-4 py-2 rounded-md hidden md:block">
         Get Started
-      </button>
-      <button className="block md:hidden" onClick={toggleMenu}>
-        menu
-      </button>
+      </Button>
     </header>
   );
 }
