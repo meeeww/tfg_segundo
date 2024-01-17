f45b1db5d3fc49da94812435792795b28a787590
Omar <118843137+Rakions@users.noreply.github.com>
2024-01-17 17:39:30 +0100

    Terminar responsive para el header

--- web/components/ui/index/header.tsx
+++ web/components/ui/index/header.tsx
@@ -12,25 +12,35 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export default function Header() {
+  const mobileMenu = () => {
+    document.querySelector(".menu")?.classList.toggle("translate-x-0");
+  };
+
   return (
     <header className="w-full h-[75px] flex items-center justify-between md:justify-center gap-10 px-4 relative">
       <h2 className="text-2xl font-bold">ChitChat</h2>
-      <ul className="gap-4 hidden md:flex">
-        <Link href={"/"} className="hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
+      <ul className="gap-4 flex md:flex-row flex-col md:static absolute top-0 left-0 w-full md:w-auto md:bg-white bg-slate-100 mt-[70px] md:mt-0 items-center justify-center translate-x-[-100%] md:translate-x-0 menu transition-all p-4">
+        <Link href={"/"} className="md:hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
           Home
         </Link>
-        <Link href={"/about"} className="hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
+        <Link href={"/about"} className="md:hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
           About us
         </Link>
-        <Link href={"/features"} className="hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
+        <Link
+          href={"/features"}
+          className="md:hover:bg-sky-200 px-2 py-1 rounded-md transition-all"
+        >
           Features
         </Link>
-        <Link href={"/contact"} className="hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
+        <Link href={"/contact"} className="md:hover:bg-sky-200 px-2 py-1 rounded-md transition-all">
           Contact
         </Link>
       </ul>
       <Button className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-4 py-2 rounded-md hidden md:block">
         Get Started
+      </Button>
+      <Button className="block md:hidden" onClick={mobileMenu}>
+        menu
       </Button>
     </header>
   );
