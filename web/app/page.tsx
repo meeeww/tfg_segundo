7d3fa1dbd1ed171936c8f73741f9b6303ba28e44
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-01-23 19:36:00 +0100

    Comenzar seccion de features index

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "./components/ui/index/header";
 import Hero from "./components/ui/index/hero";
+import IndexFeatures from "./components/ui/index/indexFeatures";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main className="px-[19px] md:px-[112px]">
         <Hero />
+        <IndexFeatures/>
       </main>
     </>
   );
